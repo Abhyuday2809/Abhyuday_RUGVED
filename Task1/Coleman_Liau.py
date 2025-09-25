1fe105c2e536f1ff4e683711923e17a11d0b3bd1
Abhyuday2809 <ABHYUDAYSHARMA28@GMAIL.COM>
2025-09-26 01:30:33 +0530

    Update Coleman_Liau.py

--- Task1/Coleman_Liau.py
+++ Task1/Coleman_Liau.py
@@ -1,11 +1,15 @@
 sen=input("Enter a string")
 sentences=0
 words=len(sen.split(' '))
-length=len(sen)
+length=0
+for i in sen:
+    if i!=' ' and i!=',' and i!='.' and i!='?':
+        length+=1
 for a in sen:
     if a=='.' or a=='!' or a=='?':
         sentences==1
 l = (length / words) * 100
 s= (sentences / words) * 100
 coleman=0.588*l-0.296*s-15.8
+
 print(f'The Coleman-Liau index for the srting "{sen}" is =  {coleman}')
