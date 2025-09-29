e1ca7c9c7b803ba67b87fa7b055a8056843dff16
Abhyuday2809 <ABHYUDAYSHARMA28@GMAIL.COM>
2025-09-29 19:27:25 +0530

    Update butterfly.py

--- Task1/butterfly.py
+++ Task1/butterfly.py
@@ -1,14 +1,21 @@
 n = int(input("Enter a number (N): "))
 
-for i in range(1, n + 1):
+for i in range(1, n):
     left = "*" * i
-    space = " " * (2 * (n - i))
+    space = " " * ((2 * (n - i))-1)
     right = "*" * i
     print(left + space + right)
 
-for i in range(n - 1, 0, -1):
-    left = "*" * i
-    space = " " * (2 * (n - i))
-    right = "*" * i
-    print(left + space + right)
+for i in range(n, 0, -1):
+    if i==n:
+        left = "*" * (i)
+        space = " " * ((2 * (n - i))-1)
+        right = "*" * (i-1)
+        print(left + space + right)
+    else:
+         left = "*" * i
+         space = " " * ((2 * (n - i))-1)
+         right = "*" * i
+         print(left + space + right)
+
 
